e360b458ed99fdad04e61f555d4e7c36ffbab5f5
lolisa <lolisa@marisa.moe>
2015-03-13 23:11:46 -0400

    Add pattern matcher

--- main.cpp
+++ main.cpp
@@ -145,9 +145,91 @@ struct pattern_matcher< constructor_indicator< self_type, which, L, R > >
                     >
                 >( s.data ).second;
 
+        static_assert(
+            std::tuple_size< typename algebraic_data_type< ARG ... >::template constructor_parameter_type< which >::type >::value == 2,
+            "Not enough constructor argument" );
         return pattern_matcher< L >::match(
                 std::get< 0 >( p ),
                 arg_loader< R, decltype( std::get< 1 >( p ) ), F > { std::get< 1 >( p ), f },
+                res ... );
+    }
+
+    template< typename ... ARG, typename F, typename ... REST >
+    static auto match( const boost::recursive_wrapper< algebraic_data_type< ARG ... > > & s, const F & f, const REST & ... res )
+    { return match( s.get( ), f, res ... ); }
+};
+
+template< size_t, typename F >
+auto expand_tuple_inner( const F & f, const std::tuple< > & )
+{ return f( ); }
+
+template< typename F, typename ... T >
+auto expand_tuple( const F & f, const std::tuple< T ... > & t )
+{ return expand_tuple_inner< 0 >( f, t ); }
+
+struct ignore_tie
+{
+    template< typename FIRST, typename ... REST >
+    auto operator ( )( const FIRST &, const REST & ... r ) const
+    { return std::tie( r ... ); }
+};
+
+template< typename ... T >
+auto tuple_pop( const std::tuple< T ... > & t )
+{ return expand_tuple( ignore_tie( ), t ); }
+
+template< typename EXP, typename STORE, typename F >
+struct multi_arg_loader
+{
+    const STORE & sec;
+    const F & f;
+    template< bool T, typename ... ARG >
+    auto helper( std::enable_if_t< T > *, const ARG & ... arg ) const
+    {
+        return pattern_matcher< typename boost::mpl::at_c< EXP, 0 >::type >::match(
+                    std::get< 0 >( sec ),
+                    multi_arg_loader< typename boost::mpl::pop_front< EXP >::type, decltype( tuple_pop( sec ) ), F > { tuple_pop( sec ), f },
+                    arg ... );
+    }
+    template< bool T, typename ... ARG >
+    auto helper( std::enable_if_t< ! T > *, const ARG & ... arg ) const
+    { return f( arg ... ); }
+    template< typename ... ARG >
+    auto operator ( )( const ARG & ... arg ) const
+    { return helper< (std::tuple_size< STORE >::value > 0), ARG ... >( nullptr, arg ... ); }
+};
+
+template< size_t nth, typename F, typename ... T, typename ... REST >
+auto expand_tuple_inner( const F & f, const std::tuple< T ... > & t, const REST & ... r )
+{
+    return misc::make_expansion(
+        [&]( const auto & t, boost::mpl::true_ ) { return f( r ..., std::get< nth >( t ) ); },
+        [&]( const auto & t, boost::mpl::false_ ) { return expand_tuple_inner< nth + 1 >( f, t, r ..., std::get< nth >( t ) ); } )
+            ( t, boost::mpl::bool_< std::tuple_size< std::tuple< T ... > >::value == nth + 1 >( ) );
+}
+
+template< typename self_type, size_t which, typename L, typename ... R >
+struct pattern_matcher< constructor_indicator< self_type, which, L, R ... > >
+{
+    template< typename ... ARG, typename F, typename ... REST >
+    static auto match( const algebraic_data_type< ARG ... > & s, const F & f, const REST & ... res )
+    {
+        auto p = boost::get
+                <
+                    std::pair
+                    <
+                        boost::mpl::int_< which >,
+                        typename algebraic_data_type< ARG ... >::template constructor_parameter_type< which >::type
+                    >
+                >( s.data ).second;
+
+        static_assert(
+            std::tuple_size< typename algebraic_data_type< ARG ... >::template constructor_parameter_type< which >::type >::value ==
+            std::tuple_size< std::tuple< R ... > >::value + 1,
+            "Not enough constructor argument" );
+        return pattern_matcher< L >::match(
+                std::get< 0 >( p ),
+                multi_arg_loader< boost::mpl::vector< R ... >, decltype( tuple_pop( p ) ), F > { tuple_pop( p ), f },
                 res ... );
     }
 
@@ -194,34 +276,6 @@ struct pattern_tester< FIRST, SECOND >
 template< >
 struct pattern_tester< >
 { static bool match_pattern( const std::tuple< > & ) { return true; } };
-
-template< size_t nth, typename F, typename ... T, typename ... REST >
-auto expand_tuple_inner( const F & f, const std::tuple< T ... > & t, const REST & ... r )
-{
-    return misc::make_expansion(
-        [&]( const auto & t, boost::mpl::true_ ) { return f( r ..., std::get< nth >( t ) ); },
-        [&]( const auto & t, boost::mpl::false_ ) { return expand_tuple_inner< nth + 1 >( f, t, r ..., std::get< nth >( t ) ); } )
-            ( t, boost::mpl::bool_< std::tuple_size< std::tuple< T ... > >::value == nth + 1 >( ) );
-}
-
-template< size_t, typename F >
-auto expand_tuple_inner( const F & f, const std::tuple< > & )
-{ return f( ); }
-
-template< typename F, typename ... T >
-auto expand_tuple( const F & f, const std::tuple< T ... > & t )
-{ return expand_tuple_inner< 0 >( f, t ); }
-
-struct ignore_tie
-{
-    template< typename FIRST, typename ... REST >
-    auto operator ( )( const FIRST &, const REST & ... r ) const
-    { return std::tie( r ... ); }
-};
-
-template< typename ... T >
-auto tuple_pop( const std::tuple< T ... > & t )
-{ return expand_tuple( ignore_tie( ), t ); }
 
 template< typename FIRST, typename ... REST >
 struct pattern_tester< FIRST, REST ... >
@@ -304,7 +358,7 @@ struct matcher
 struct unit { }; //Fuck void
 typedef algebraic_data_type< unit, unit > Bool;
 typedef algebraic_data_type< unit, recursive_indicator > Nat;
-typedef algebraic_data_type< std::tuple< bool, bool > > pair_bool;
+typedef algebraic_data_type< std::tuple< bool, bool, bool > > tri_bool;
 #define DECLARE_CONSTRUCTOR( ADT, WHICH, NAME, UNUSED ) \
 template< typename ... UNUSED > \
 using NAME = typename ADT::get_constructor< WHICH, UNUSED ... >::type
@@ -313,12 +367,12 @@ DECLARE_CONSTRUCTOR( Bool, 0, True, T );
 DECLARE_CONSTRUCTOR( Bool, 1, False, T );
 DECLARE_CONSTRUCTOR( Nat, 0, O, T );
 DECLARE_CONSTRUCTOR( Nat, 1, S, T );
-DECLARE_CONSTRUCTOR( pair_bool, 0, pb, T );
+DECLARE_CONSTRUCTOR( tri_bool, 0, tb, T );
 
 int main( )
 {
-    pair_bool p = pb<>( )( std::make_pair( true, false ) );
-    assert( ( p.match< pb< arg, arg > >( []( bool l, bool r ) { return l && ! r; } ) ) );
+    tri_bool p = tb<>( )( std::make_tuple( true, false, false ) );
+    assert( ( p.match< tb< arg, arg, wildstar > >( []( bool l, bool r ) { return l && ! r; } ) ) );
     std::cout << "pass!" << std::endl;
     return 0;
 }
