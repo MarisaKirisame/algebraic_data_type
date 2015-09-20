9e7da599cb40ab1ddb3640d89261db3068c7dd16
lolisa <lolisa@marisa.moe>
2015-09-19 20:30:41 -0700

    make syntax shorter

--- test.hpp
+++ test.hpp
@@ -12,7 +12,7 @@ DECLARE_CONSTRUCTOR( Nat, 1, O, T );
 DECLARE_CONSTRUCTOR( Nat, 0, S, T );
 BOOST_AUTO_TEST_CASE( nat_test )
 {
-    Nat n = S<>( )( std::make_tuple( S<>( )( std::make_tuple( O<>( )( std::make_tuple( unit( ) ) ) ) ) ) );
+    Nat n = S<>( )( S<>( )( O<>( )( unit( ) ) ) );
     BOOST_CHECK( n.match_pattern< S< arg > >( ) );
     BOOST_CHECK( n.match_pattern< S< S< arg > > >( ) );
     BOOST_CHECK( (
@@ -27,7 +27,7 @@ DECLARE_CONSTRUCTOR( Bool, 1, False, T );
 DECLARE_CONSTRUCTOR( Bool, 0, True, T );
 BOOST_AUTO_TEST_CASE( bool_test )
 {
-    Bool b = True<>( )( std::make_tuple( unit( ) ) );
+    Bool b = True<>( )( unit( ) );
     BOOST_CHECK( b.match_pattern< wildstar >( ) );
     BOOST_CHECK( b.match_pattern< True< arg > >( ) );
     BOOST_CHECK( ( b.match< False< wildstar >, True< wildstar > >( []( ) { return true; } ) ) );
@@ -37,7 +37,7 @@ typedef algebraic_data_type< std::tuple< bool, bool, bool > > tri_bool;
 DECLARE_CONSTRUCTOR( tri_bool, 0, tb, T );
 BOOST_AUTO_TEST_CASE( tri_bool_test )
 {
-    tri_bool p = tb<>( )( std::make_tuple( true, false, false ) );
+    tri_bool p = tb<>( )( true, false, false );
     BOOST_CHECK( ( p.match< tb< arg, arg, wildstar > >( []( bool l, bool r ) { return l && ! r; } ) ) );
 }
 
@@ -46,7 +46,7 @@ DECLARE_CONSTRUCTOR( bl, 1, nil, t );
 DECLARE_CONSTRUCTOR( bl, 0, cons, t );
 BOOST_AUTO_TEST_CASE( bl_test )
 {
-    bl l = cons< >( )( std::make_tuple( true, nil< >( )( std::make_tuple( unit( ) ) ) ) );
+    bl l = cons< >( )( true, nil< >( )( unit( ) ) );
     BOOST_CHECK( ( l.match< cons< arg, arg > >( []( bool b, const bl & ){ return b; } ) ) );
 }
 
@@ -54,7 +54,7 @@ typedef algebraic_data_type< std::tuple< Bool, Bool > > meow;
 DECLARE_CONSTRUCTOR( meow, 0, Meow, t );
 BOOST_AUTO_TEST_CASE( meow_test )
 {
-    meow MEOW = Meow< >( )( std::make_tuple( True<>( )( std::make_tuple( unit( ) ) ), False<>( )( std::make_tuple( unit( ) ) ) ) );
+    meow MEOW = Meow< >( )( True<>( )( unit( ) ), False<>( )( unit( ) ) );
     BOOST_CHECK( ( MEOW.match< Meow< True< wildstar >, False< wildstar > > >( []( ) { return true; } ) ) );
 }
 
