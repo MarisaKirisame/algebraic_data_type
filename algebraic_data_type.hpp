a42f92170b97a9f11830f762f6f1b630d0a8bb07
lolisa <lolisa@marisa.moe>
2015-10-07 23:13:02 -0700

    add DECLARE_CONSTRUCTORS

--- algebraic_data_type.hpp
+++ algebraic_data_type.hpp
@@ -3,8 +3,29 @@
 #include "pattern_tester.hpp"
 #include "core.hpp"
 #include "pattern_matcher.hpp"
-#define DECLARE_CONSTRUCTOR( ADT, WHICH, NAME, UNUSED ) \
-    template< typename ... UNUSED > \
-    auto NAME( UNUSED && ... UNUSED ## 0 ) { return constructor< ADT, WHICH >( std::forward< UNUSED >( UNUSED ## 0 ) ... ); }
-    typedef std::tuple< > unit; //Fuck void
+#include <boost/preprocessor.hpp>
+#define DECLARE_CONSTRUCTOR( ADT, WHICH, NAME, FRESH ) \
+    template< typename ... FRESH > \
+    auto NAME( FRESH && ... BOOST_PP_CAT( FRESH, 0 ) ) { return constructor< ADT, WHICH >( std::forward< FRESH >( BOOST_PP_CAT( FRESH, 0 ) ) ... ); }
+
+#define DECLARE_CONSTRUCTORS_AUX( r, data, which, name ) \
+    DECLARE_CONSTRUCTOR( BOOST_PP_SEQ_ELEM( 0, data ), which, name, BOOST_PP_SEQ_ELEM( 1, data ) )
+
+#define DECLARE_CONSTRUCTORS( ADT, NAMES, FRESH ) \
+    BOOST_PP_SEQ_FOR_EACH_I_R( 1, DECLARE_CONSTRUCTORS_AUX, (ADT)(FRESH), BOOST_PP_TUPLE_TO_SEQ( NAMES ) )
+
+#define SEQ_OBJECT( SEQ ) \
+    BOOST_PP_TUPLE_REM_CTOR( \
+        BOOST_PP_IF( BOOST_PP_EQUAL( 1, BOOST_PP_SEQ_SIZE( SEQ ) ), ( BOOST_PP_SEQ_ELEM( 0, SEQ ) ), ( std::tuple< BOOST_PP_SEQ_ENUM( SEQ ) > ) ) )
+
+#define DECLARE_ADT_AUX_0( s, data, elem ) BOOST_PP_TUPLE_ELEM( 0, elem )
+
+#define DECLARE_ADT_AUX_1( s, data, elem ) BOOST_PP_TUPLE_POP_FRONT( elem )
+
+#define DECLARE_ADT_AUX_2( s, data, elem ) SEQ_OBJECT( elem )
+
+#define DECLARE_ADT( ADT, CONSTRUCTORS, FRESH ) \
+    BOOST_PP_TUPLE_TO_SEQ( DECLARE_ADT_AUX_0, _, CONSTRUCTORS ) //DECLARE_CONSTRUCTORS( ADT, BOOST_PP_TUPLE_TRANSFORM( DECLARE_ADT_AUX_0, _, CONSTRUCTORS ), FRESH )
+
+namespace algebraic_data_type { typedef std::tuple< > unit; } //Fuck void
 #endif // ALGEBRAIC_DATA_TYPE_HPP
--- test.hpp
+++ test.hpp
@@ -4,16 +4,13 @@
 #define BOOST_TEST_MAIN
 #include <boost/test/included/unit_test.hpp> //#define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
-
 using namespace algebraic_data_type;
 
 typedef algebraic_data_type< recursive_indicator, unit > Nat;
-DECLARE_CONSTRUCTOR( Nat, 1, O, T )
-DECLARE_CONSTRUCTOR( Nat, 0, S, T )
+DECLARE_CONSTRUCTORS( Nat, (S, O), T )
 
 BOOST_AUTO_TEST_CASE( nat_test )
 {
-
     Nat n = S( S( O( ) ) );
     BOOST_CHECK( n.match_pattern( S( arg ) ) );
     BOOST_CHECK( (
@@ -47,13 +44,11 @@ BOOST_AUTO_TEST_CASE( tri_bool_test )
     BOOST_CHECK( ( p.match( with( tb( arg, arg, wildstar ), []( bool l, bool r ) { return l && ! r; } ) ) ) );
 }
 
-typedef algebraic_data_type< std::tuple< bool, recursive_indicator >, unit > bl;
-DECLARE_CONSTRUCTOR( bl, 1, nil, t )
-DECLARE_CONSTRUCTOR( bl, 0, cons, t )
 BOOST_AUTO_TEST_CASE( bl_test )
 {
-    bl l = cons( true, nil( ) );
-    BOOST_CHECK( ( l.match( with( cons( arg, arg ), []( bool b, const bl & ){ return b; } ) ) ) );
+    //std::cout << BOOST_PP_STRINGIZE( ( DECLARE_ADT( bl, ((cons,bool), (nil,unit)), t ) ) );
+    //bl l = cons( true, nil( ) );
+    //BOOST_CHECK( ( l.match( with( cons( arg, arg ), []( bool b, const bl & ){ return b; } ) ) ) );
 }
 
 typedef algebraic_data_type< std::tuple< Bool, Bool > > meow;
